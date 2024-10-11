a86cfea5c17a1856980991b34297e363dccb4159
Hai Duong <dohaiduong1803@gmail.com>
2024-10-11 08:41:16 +0700

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,12 @@ import '@nomicfoundation/hardhat-toolbox'
 import * as dotenv from 'dotenv'
 dotenv.config({ path: __dirname + '/.env' })
 const config: HardhatUserConfig = {
-  solidity: '0.8.19',
+  solidity: '0.8.26',
   networks: {
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/c57eb9c303154af2b82bc91705e80c22`,
+      accounts: [`0x${process.env.PRIVATE_KEY_MAINNET}`],
+    },
     amoy: {
       url: `https://rpc-amoy.polygon.technology`,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
@@ -14,5 +18,3 @@ const config: HardhatUserConfig = {
     apiKey: process.env.API_KEY
   }
 }
-
-export default config
